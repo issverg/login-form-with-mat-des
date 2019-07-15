1b262abcc3e34dfaf21b51c21e22f819590d8d9e
issverg <issverg@gmail.com>
2019-07-15 17:26:00 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Response from './components/Response';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: ''
+        };
+    }
+
+    getToken = (value) => {
+        this.setState({ response: value })
+    };
+
+    render() {
+        return (
+            <div className = "App">
+                <Form getToken = { this.getToken }/>
+                <Response token = { this.state.response } />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//injectTapEventPlugin();
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
